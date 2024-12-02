4571a70db5545485089bbfc2f2fb883888e5c2ec
LeaderbotX400 <eric@singerfamily.ca>
2024-12-02 10:01:43 -0700

    Remove Spotify and Steam modules; refactor PipeWire configuration for improved defaults

--- modules/nixos/core/sound/default.nix
+++ modules/nixos/core/sound/default.nix
@@ -1,26 +1,14 @@
-{ lib, config, ... }:
-let
-  cfg = config.services.pipewire;
-in
-{
-  config = lib.mkIf cfg.enable {
-    security.rtkit.enable = lib.mkForce true;
-    # hardware = {
-    #   pulseaudio = {
-    #     enable = true;
-    #     systemWide = true;
-    #   };
-    # };
-    hardware.pulseaudio.enable = lib.mkForce false;
-    services.pipewire = {
-      enable = lib.mkForce true;
-      systemWide = true;
-      alsa = {
-        enable = lib.mkForce true;
-        support32Bit = lib.mkForce true;
-      };
-      pulse.enable = lib.mkForce true;
-      jack.enable = lib.mkForce true;
+{ lib, ... }: {
+  security.rtkit.enable = lib.mkForce true;
+  hardware.pulseaudio.enable = lib.mkForce false;
+  services.pipewire = {
+    enable = lib.mkDefault true;
+    systemWide = lib.mkDefault true;
+    alsa = {
+      enable = lib.default true;
+      support32Bit = lib.default true;
     };
+    pulse.enable = lib.default true;
+    jack.enable = lib.default true;
   };
 }
