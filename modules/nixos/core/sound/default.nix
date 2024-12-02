a3e40a2fcee64578eaf2c3ec39b5ad22be4f4a5f
LeaderbotX400 <eric@singerfamily.ca>
2024-12-02 09:49:25 -0700

    switch to pipewire

--- modules/nixos/core/sound/default.nix
+++ modules/nixos/core/sound/default.nix
@@ -5,20 +5,20 @@ in
 {
   config = lib.mkIf cfg.enable {
     security.rtkit.enable = lib.mkForce true;
-    hardware = {
-      pulseaudio = {
-        enable = true;
-        systemWide = true;
-      };
-    };
-    # hardware.pulseaudio.enable = lib.mkForce false;
-    # services.pipewire = {
-    #   alsa = {
-    #     enable = lib.mkForce true;
-    #     support32Bit = lib.mkForce true;
+    # hardware = {
+    #   pulseaudio = {
+    #     enable = true;
+    #     systemWide = true;
     #   };
-    #   pulse.enable = lib.mkForce true;
-    #   jack.enable = lib.mkForce true;
     # };
+    hardware.pulseaudio.enable = lib.mkForce false;
+    services.pipewire = {
+      alsa = {
+        enable = lib.mkForce true;
+        support32Bit = lib.mkForce true;
+      };
+      pulse.enable = lib.mkForce true;
+      jack.enable = lib.mkForce true;
+    };
   };
 }
