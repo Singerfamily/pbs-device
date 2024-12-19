447c7d6b0e8a323313422e7c880c2308d28b8b08
LeaderbotX400 <eric@singerfamily.ca>
2024-12-19 10:48:30 -0700

    Bump flake deps

--- hosts/seton/default.nix
+++ hosts/seton/default.nix
@@ -4,8 +4,12 @@
     ./hardware-configuration.nix
   ];
 
-  services.udev.extraRules = ''
-    ACTION=="add", SUBSYSTEM=="video4linux", KERNEL=="video0", RUN+="${pkgs.docker}/bin/docker compose -f /home/${username}/obs/docker-compose.yml up -d"
-    ACTION=="remove", SUBSYSTEM=="video4linux", KERNEL=="video0", RUN+="${pkgs.docker}/bin/docker compose -f /home/${username}/obs/docker-compose.yml down"
-  '';
+  services.udev.extraRules =
+    let
+      obsPath = "/home/${username}/obs";
+    in
+    ''
+      ACTION=="add", SUBSYSTEM=="video4linux", KERNEL=="video0", RUN+="${pkgs.docker}/bin/docker compose -f ${obsPath}/docker-compose.yml up -d"
+      ACTION=="remove", SUBSYSTEM=="video4linux", KERNEL=="video0", RUN+="${pkgs.docker}/bin/docker compose -f ${obsPath}/docker-compose.yml down"
+    '';
 }
