a4dc53bf87c77aa3816277873ade352b1682bbd0
LeaderbotX400 <eric@singerfamily.ca>
2024-12-19 10:26:10 -0700

    Refactor Docker service configuration: simplify user group assignment and enable auto-prune

--- modules/nixos/services/docker/default.nix
+++ modules/nixos/services/docker/default.nix
@@ -5,26 +5,13 @@
   username,
   ...
 }:
-let
-  cfg = config.virtualisation.docker;
-in
 {
-  config = lib.mkIf cfg.enable {
-    # hardware.nvidia-container-toolkit.enable = true;
-
-    users.users.${username}.extraGroups = [ "docker" ];
-    virtualisation.docker = {
-      rootless = {
-        enable = true;
-        setSocketVariable = true;
-      };
-      autoPrune.enable = true;
-    };
-
-    # Useful other development tools
-    # environment.systemPackages = with pkgs; [
-    #   distrobox
-    #   dive # look into docker image layers
-    # ];
+  users.users.${username}.extraGroups = [ "docker" ];
+  virtualisation.docker = {
+    # rootless = {
+    #   enable = true;
+    #   setSocketVariable = true;
+    # };
+    autoPrune.enable = true;
   };
 }
