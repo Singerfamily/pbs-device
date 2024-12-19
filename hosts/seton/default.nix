95d41ecb85d916c2ae283c5aaf4f2a251112397e
LeaderbotX400 <eric@singerfamily.ca>
2024-12-19 10:26:14 -0700

    Add udev rules for automatic Docker management of video devices

--- hosts/seton/default.nix
+++ hosts/seton/default.nix
@@ -1,6 +1,11 @@
-{ ... }:
+{ pkgs, username, ... }:
 {
   imports = [
     ./hardware-configuration.nix
   ];
+
+  services.udev.extraRules = ''
+    ACTION=="add", SUBSYSTEM=="video4linux", KERNEL=="video0", RUN+="${pkgs.docker}/bin/docker compose -f /home/${username}/obs/docker-compose.yml up -d"
+    ACTION=="remove", SUBSYSTEM=="video4linux", KERNEL=="video0", RUN+="${pkgs.docker}/bin/docker compose -f /home/${username}/obs/docker-compose.yml down"
+  '';
 }
