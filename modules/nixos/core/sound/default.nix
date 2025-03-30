b33e79b72dabdbc549bff690d1d2a3dfbfb5c4e6
LeaderbotX400 <eric@singerfamily.ca>
2025-03-30 14:19:49 -0600

    fix(sound): update PulseAudio and ALSA configurations in NixOS modules

--- modules/nixos/core/sound/default.nix
+++ modules/nixos/core/sound/default.nix
@@ -2,9 +2,10 @@
 {
   security.rtkit.enable = lib.mkForce true;
   # hardware.pulseaudio.enable = lib.mkForce false;
+  hardware.alsa.enablePersistence = lib.mkDefault true;
   services.pipewire = {
     enable = lib.mkDefault true;
-    # systemWide = lib.mkDefault true;
+    systemWide = lib.mkDefault true;
     alsa = {
       enable = lib.mkDefault true;
       support32Bit = lib.mkDefault true;
